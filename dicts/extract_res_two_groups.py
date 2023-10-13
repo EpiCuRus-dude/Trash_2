ba7c1a397472bc349c7772bd71ffbeff2274bcd7
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-13 10:16:51 -0700

    Update extract_res_two_groups.py

--- dicts/extract_res_two_groups.py
+++ dicts/extract_res_two_groups.py
@@ -1,17 +1,23 @@
-# Update the group_labels dictionary
+# Updated group_labels dictionary
 group_labels = {
-    1: [{'apple': '1'}, {'banana': '2'}, {'grapes': '4'}],
-    2: [{'orange': '5'}, {'peach': '6'}, {'pear': '7'}, {'orange': '8'}]
+    1: {
+        'A': [{'apple': '1'}, {'banana': '2'}, {'grapes': '4'}],
+        'B': ['some_stuff']
+    },
+    2: {
+        'A': [{'orange': '5'}, {'peach': '6'}, {'pear': '7'}, {'orange': '8'}],
+        'B': []
+    }
 }
 
-# Re-flatten the group_labels for easier searching
-flattened_group_labels = {group: {k: v for d in dicts for k, v in d.items()} for group, dicts in group_labels.items()}
+# Initialize dictionaries for each group
+group_1_dict = {'true': [], 'pred': [], 'ids': [], 'general_ids': []}
+group_2_dict = {'true': [], 'pred': [], 'ids': [], 'general_ids': []}
 
-# Initialize dictionaries for each group again
-group_1_dict = {'true': [], 'pred': [], 'ids': []}
-group_2_dict = {'true': [], 'pred': [], 'ids': []}
+# Flatten the 'A' part of group_labels for easier searching
+flattened_group_labels_A = {group: {k: v for d in dicts['A'] for k, v in d.items()} for group, dicts in group_labels.items()}
 
-# Re-populate group dictionaries
+# Populate group dictionaries
 for i in range(len(A['true'])):
     true_label, pred_label = A['true'][i], A['pred'][i]
     
@@ -31,8 +37,8 @@ for i in range(len(A['true'])):
     elif group == 2:
         group_2_dict['pred'].append(pred_label)
 
-# Add general IDs to the dictionaries
-group_1_dict['general_ids'] = list(flattened_group_labels[1].values())
-group_2_dict['general_ids'] = list(flattened_group_labels[2].values())
+# Add general IDs
+group_1_dict['general_ids'] = list(flattened_group_labels_A[1].values())
+group_2_dict['general_ids'] = list(flattened_group_labels_A[2].values())
 
 group_1_dict, group_2_dict
