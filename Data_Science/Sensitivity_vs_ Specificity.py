cde00cad44bfa192c217944b76cba49cb0862874
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-16 09:05:03 -0700

    Update Sensitivity_vs_ Specificity.py

--- Data_Science/Sensitivity_vs_ Specificity.py
+++ Data_Science/Sensitivity_vs_ Specificity.py
@@ -25,3 +25,45 @@ def specificity(true_class, predicted_class, target_class):
 for label in class_labels:
     spec = specificity(df['True Class'], df['Predicted Class'], label)
     print(f'Specificity for Class {label}: {spec:.2f}')
+
+
+#Example II
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+# Generate some example data for a three-class problem
+np.random.seed(0)
+true_labels = np.random.randint(0, 3, 100)
+predicted_scores = np.random.rand(100, 3)  # Simulated predicted scores (probabilities)
+
+# Create an array of different thresholds
+thresholds = np.linspace(0, 1, 101)
+
+# Initialize lists to store sensitivity and specificity values
+sensitivity_list = []
+specificity_list = []
+
+# Calculate sensitivity and specificity for each threshold
+for threshold in thresholds:
+    predicted_labels = (predicted_scores >= threshold).argmax(axis=1)  # Thresholding
+    cm = confusion_matrix(true_labels, predicted_labels)
+    
+    # Sensitivity (True Positive Rate)
+    sensitivity = cm[1, 1] / (cm[1, 1] + cm[1, 0])
+    sensitivity_list.append(sensitivity)
+    
+    # Specificity (True Negative Rate)
+    specificity = cm[0, 0] / (cm[0, 0] + cm[0, 1] + cm[0, 2] + cm[2, 0] + cm[2, 1] + cm[2, 2])
+    specificity_list.append(specificity)
+
+# Plot sensitivity and specificity vs. threshold
+plt.figure(figsize=(10, 5))
+plt.plot(thresholds, sensitivity_list, label='Sensitivity (True Positive Rate)')
+plt.plot(thresholds, specificity_list, label='Specificity (True Negative Rate)')
+plt.xlabel('Threshold')
+plt.ylabel('Score')
+plt.title('Sensitivity and Specificity vs. Threshold for a Three-Class Problem')
+plt.legend()
+plt.grid(True)
+plt.show()
