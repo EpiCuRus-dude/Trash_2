751d51ad137da2d59bee309731fbe1dc6468b53d
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-17 16:06:18 -0700

    Update quantile_bazi.py

--- Data_Science/quantile_bazi.py
+++ Data_Science/quantile_bazi.py
@@ -1,5 +1,5 @@
 df = pd.DataFrame({
-    'A': ['a', 'b', 'a', 'b', 'b', 'c', 'c','b', 'd', 'd' 'e', 'e','b', 'd', 'd','b', 'd', 'd', 'b', 'd', 'd', 'b', 'c', 'c','b', 'd', 'd' 'e']
+    'A': ['a', 'b', 'a', 'b', 'b', 'c', 'c','b', 'd', 'd' ,'e', 'e','b', 'd', 'd','b', 'd', 'd', 'b', 'd', 'd', 'b', 'c', 'c','b', 'd', 'd' ,'e']
 })
 
 label_counts = df['A'].value_counts()
@@ -9,6 +9,9 @@ Xth_quantile = label_counts_sorted.quantile(0.25)
 Yth_quantile = label_counts_sorted.quantile(0.55) 
 
 
-group1_labels = label_counts_sorted[label_counts_sorted>=Yth_quantile]
-group2_labels = label_counts_sorted[(label_counts_sorted>=Xth_quantile) & (label_counts_sorted<Yth_quantile)]
+group1_labels = label_counts_sorted[label_counts_sorted>=Yth_quantile].index.tolist()
+group2_labels = label_counts_sorted[(label_counts_sorted>=Xth_quantile) & (label_counts_sorted<Yth_quantile)].index.tolist()
+
+
+group2_labels_counts = label_counts_sorted[(label_counts_sorted>=Xth_quantile) & (label_counts_sorted<Yth_quantile)]
 
