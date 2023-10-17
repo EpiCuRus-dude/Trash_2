ea3898f929c361eceb73b609c5224e174991223e
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-17 16:09:51 -0700

    Update quantile_bazi.py

--- Data_Science/quantile_bazi.py
+++ Data_Science/quantile_bazi.py
@@ -22,3 +22,15 @@ g2_Yth_quantile = group2_labels_counts_sorted.quantile(0.55)
 g2_group1_labels = group2_labels_counts_sorted[group2_labels_counts_sorted>=g2_Yth_quantile].index.tolist()
 g2_group2_labels = group2_labels_counts_sorted[(group2_labels_counts_sorted>=g2_Xth_quantile) & (group2_labels_counts_sorted<g2_Yth_quantile)].index.tolist()
 g2_group3_labels = group2_labels_counts_sorted[group2_labels_counts_sorted<g2_Xth_quantile].index.tolist()
+
+
+
+# Create final dictionary
+result = {
+    'Group 1': group1_labels,
+    'Group 2': {
+        'A': g2_group1_labels,
+        'B': g2_group2_labels,
+        'C': g2_group3_labels
+    }
+}
