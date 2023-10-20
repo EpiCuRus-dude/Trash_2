7eb54169a7f77b5e877c3e04f8cbc63fd9f76a1f
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-20 15:07:22 -0700

    Update Data_Pipe_line_Zamani.py

--- Data_Science/Data_Pipe_line_Zamani.py
+++ Data_Science/Data_Pipe_line_Zamani.py
@@ -1,65 +1,32 @@
 import pandas as pd
-import numpy as np
 import random
 from datetime import datetime, timedelta
 
-# Number of rows in the DataFrame
-num_rows = 100
+# Function to generate random date between two dates
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60)
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
 
-# Generate 5 classes randomly distributed
-labels = ['Class_' + str(i+1) for i in range(5)]
-class_column = [random.choice(labels) for _ in range(num_rows)]
-
-# Generate random dates between January 2020 and March 2020
+# Define the date range
 start_date = datetime(2020, 1, 1)
 end_date = datetime(2020, 3, 31)
 
-# Create a function to generate a random date within the given range
-def random_date(start_date, end_date):
-    delta = end_date - start_date
-    random_days = random.randint(0, delta.days)
-    return start_date + timedelta(days=random_days)
+# Number of samples
+num_samples = 1000
 
-date_column = [random_date(start_date, end_date) for _ in range(num_rows)]
+# Generate random classes (labels) and dates
+random_classes = [f'Class {random.randint(1, 5)}' for _ in range(num_samples)]
+random_dates = [random_date(start_date, end_date) for _ in range(num_samples)]
 
 # Create the DataFrame
 df = pd.DataFrame({
-    'Label': class_column,
-    'Date': date_column
+    'Class': random_classes,
+    'Date': random_dates
 })
 
-# Sort the DataFrame by Date for better visibility
-df.sort_values(by='Date', inplace=True)
-df.reset_index(drop=True, inplace=True)
+# Sort the DataFrame by date for better readability
+df = df.sort_values('Date').reset_index(drop=True)
 
-df.head(10)  # Show the first 10 rows of the DataFrame
-
-
-# Generate more rows for March 2020 to September 2020
-extended_num_rows = 200  # Number of additional rows to be added
-
-# Add labels for Class_6 and Class_7
-extended_labels = ['Class_' + str(i+1) for i in range(7)]
-extended_class_column = [random.choice(extended_labels) for _ in range(extended_num_rows)]
-
-# Generate random dates between March 2020 and September 2020
-extended_start_date = datetime(2020, 3, 1)
-extended_end_date = datetime(2020, 9, 30)
-
-extended_date_column = [random_date(extended_start_date, extended_end_date) for _ in range(extended_num_rows)]
-
-# Create the extended DataFrame
-extended_df = pd.DataFrame({
-    'Label': extended_class_column,
-    'Date': extended_date_column
-})
-
-# Concatenate the original and extended DataFrames
-final_df = pd.concat([df, extended_df], ignore_index=True)
-
-# Sort the final DataFrame by Date
-final_df.sort_values(by='Date', inplace=True)
-final_df.reset_index(drop=True, inplace=True)
-
-final_df.head(10)  # Show the first 10 rows of the final DataFrame
-
+df.head(10)  # Display the first 10 rows
