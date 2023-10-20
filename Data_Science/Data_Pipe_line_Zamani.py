d002c20286ed06f34e56a61973fbf0e320a871c3
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-20 14:52:46 -0700

    Update Data_Pipe_line_Zamani.py

--- Data_Science/Data_Pipe_line_Zamani.py
+++ Data_Science/Data_Pipe_line_Zamani.py
@@ -33,3 +33,33 @@ df.sort_values(by='Date', inplace=True)
 df.reset_index(drop=True, inplace=True)
 
 df.head(10)  # Show the first 10 rows of the DataFrame
+
+
+# Generate more rows for March 2020 to September 2020
+extended_num_rows = 200  # Number of additional rows to be added
+
+# Add labels for Class_6 and Class_7
+extended_labels = ['Class_' + str(i+1) for i in range(7)]
+extended_class_column = [random.choice(extended_labels) for _ in range(extended_num_rows)]
+
+# Generate random dates between March 2020 and September 2020
+extended_start_date = datetime(2020, 3, 1)
+extended_end_date = datetime(2020, 9, 30)
+
+extended_date_column = [random_date(extended_start_date, extended_end_date) for _ in range(extended_num_rows)]
+
+# Create the extended DataFrame
+extended_df = pd.DataFrame({
+    'Label': extended_class_column,
+    'Date': extended_date_column
+})
+
+# Concatenate the original and extended DataFrames
+final_df = pd.concat([df, extended_df], ignore_index=True)
+
+# Sort the final DataFrame by Date
+final_df.sort_values(by='Date', inplace=True)
+final_df.reset_index(drop=True, inplace=True)
+
+final_df.head(10)  # Show the first 10 rows of the final DataFrame
+
