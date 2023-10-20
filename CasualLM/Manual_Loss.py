4f5ad28beae3a3638e5e49155fc12f9e68605a7b
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-20 11:53:08 -0700

    Update Manual_Loss.py

--- CasualLM/Manual_Loss.py
+++ CasualLM/Manual_Loss.py
@@ -1,1 +1,23 @@
 ### Here
+
+
+import torch
+from torch.nn import CrossEntropyLoss
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained('gpt2')
+model = AutoModelForCausalLM.from_pretrained('gpt2')
+
+# Define the input sentence and labels
+sentence = "I love black"
+labels = "cats"
+
+
+# Tokenize the inputs and labels
+inputs = tokenizer(sentence, return_tensors='pt', truncation=True)
+labels_tokens = tokenizer(labels, return_tensors='pt', truncation=True)['input_ids']
+
+# Extend the input_ids tensor to match the labels tensor size
+input_ids = torch.cat([inputs['input_ids'], labels_tokens], dim=1)
+
