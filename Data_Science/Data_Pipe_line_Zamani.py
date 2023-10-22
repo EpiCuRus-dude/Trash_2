d6323204c0e83b03978cd072b89045a3a9c70199
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-22 10:18:08 -0700

    Update Data_Pipe_line_Zamani.py

--- Data_Science/Data_Pipe_line_Zamani.py
+++ Data_Science/Data_Pipe_line_Zamani.py
@@ -1,59 +1,35 @@
 import pandas as pd
-import random
+import numpy as np
 from datetime import datetime, timedelta
 
-# Function to generate random date between two dates
-def random_date(start, end):
-    delta = end - start
-    int_delta = (delta.days * 24 * 60 * 60)
-    random_second = random.randrange(int_delta)
-    return start + timedelta(seconds=random_second)
+# Generate random data for Jan 2020 to March 2020 (Classes 1 to 5)
+start_date1 = datetime.strptime("2020-01-01", "%Y-%m-%d")
+end_date1 = datetime.strptime("2020-03-31", "%Y-%m-%d")
+delta1 = end_date1 - start_date1
 
-# Define the date range
-start_date = datetime(2020, 1, 1)
-end_date = datetime(2020, 3, 31)
+data1 = {
+    "date": [start_date1 + timedelta(days=np.random.randint(0, delta1.days)) for _ in range(200)],
+    "class": np.random.randint(1, 6, 200)  # Classes from 1 to 5
+}
 
-# Number of samples
-num_samples = 1000
+df1 = pd.DataFrame(data1)
+df1 = df1.sort_values(by="date")
 
-# Generate random classes (labels) and dates
-random_classes = [f'Class {random.randint(1, 5)}' for _ in range(num_samples)]
-random_dates = [random_date(start_date, end_date) for _ in range(num_samples)]
+# Generate random data for March 2020 to Sep 2020 (Classes 1 to 7)
+start_date2 = datetime.strptime("2020-03-01", "%Y-%m-%d")
+end_date2 = datetime.strptime("2020-09-30", "%Y-%m-%d")
+delta2 = end_date2 - start_date2
 
-# Create the DataFrame
-df = pd.DataFrame({
-    'Class': random_classes,
-    'Date': random_dates
-})
+data2 = {
+    "date": [start_date2 + timedelta(days=np.random.randint(0, delta2.days)) for _ in range(300)],
+    "class": np.random.randint(1, 8, 300)  # Classes from 1 to 7
+}
 
-# Sort the DataFrame by date for better readability
-df = df.sort_values('Date').reset_index(drop=True)
+df2 = pd.DataFrame(data2)
+df2 = df2.sort_values(by="date")
 
-df.head(10)  # Display the first 10 rows
+# Combine both dataframes
+df = pd.concat([df1, df2], ignore_index=True)
 
-# Update the date range for the new set of data
-start_date_new = datetime(2020, 4, 1)
-end_date_new = datetime(2020, 9, 30)
-
-# Number of new samples
-num_samples_new = 1500
-
-# Generate random classes (labels) and dates for the new set of data
-random_classes_new = [f'Class {random.randint(1, 7)}' for _ in range(num_samples_new)]
-random_dates_new = [random_date(start_date_new, end_date_new) for _ in range(num_samples_new)]
-
-# Create a new DataFrame for the new set of data
-df_new = pd.DataFrame({
-    'Class': random_classes_new,
-    'Date': random_dates_new
-})
-
-# Combine the old and new DataFrames
-df_combined = pd.concat([df, df_new], ignore_index=True)
-
-# Sort the combined DataFrame by date
-df_combined = df_combined.sort_values('Date').reset_index(drop=True)
-
-df_combined.tail(10)  # Display the last 10 rows to show the new data
-
-
+# Show the first few rows of the dataframe
+df.head()
