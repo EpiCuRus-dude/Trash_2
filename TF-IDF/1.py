2f8bf8f168ad82532c70c23002ab58031392391e
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-24 17:14:04 -0700

    Update 1.py

--- TF-IDF/1.py
+++ TF-IDF/1.py
@@ -3,3 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(cleaned_texts)
+
+
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(labels)
