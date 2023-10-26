07f7b24da34a1aac8ef1aa365f6ce86e40e4d3b3
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-10-26 16:06:09 -0700

    Update weighted_sampling.py

--- Data_Science/weighted_sampling.py
+++ Data_Science/weighted_sampling.py
@@ -13,3 +13,13 @@ start_date = '2021-01-20'
 end_date = '2021-04-01'
 
 filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+
+decay_rate = 0.1  
+most_recent_date = filtered_df['Date'].max()
+
+
+filtered_df['DaysFromRecent'] = (most_recent_date - filtered_df['Date']).dt.days
+
+
+filtered_df['Weight'] = np.exp(-decay_rate * filtered_df['DaysFromRecent'])
