402bf51f3feb5fda79cb4ce3bfbab55870e052b9
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-11-20 12:32:23 -0800

    Update legend.py

--- VAE/legend.py
+++ VAE/legend.py
@@ -12,3 +12,31 @@ plt.tight_layout()
 
 
 plt.show()
+
+
+for label in encoded_data:
+    encoded_data[label] = np.array(encoded_data[label])
+
+# Dictionary for outliers
+outliers = {label: [] for label in range(14)}
+
+# Define a threshold for Z-score, e.g., 3
+threshold = 3
+
+
+for label, data in encoded_data.items():
+    if len(data) == 0:
+        continue
+
+    
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+
+    
+    z_scores = np.abs((data - mean) / std)
+
+    
+    outlier_indices = np.where(np.any(z_scores > threshold, axis=1))[0]
+
+    
+    outliers[label] = data[outlier_indices]
