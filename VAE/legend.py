3cd186d1c94d874af65b2edb5198385752c9678a
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-11-20 11:50:27 -0800

    Update legend.py

--- VAE/legend.py
+++ VAE/legend.py
@@ -3,3 +3,12 @@ legend_labels = [f'Label {label}: Color {color}' for label, color in label_color
 handles = [plt.Line2D([0], [0], marker='o', color='w', label=lbl,
                       markerfacecolor=color, markersize=10) for lbl, color in zip(legend_labels, label_color_map.values())]
 ax.legend(handles=handles, title='Label Colors')
+
+
+ax.legend(handles=legend_handles, title='Labels', loc='upper left', bbox_to_anchor=(1, 1))
+
+
+plt.tight_layout()
+
+
+plt.show()
