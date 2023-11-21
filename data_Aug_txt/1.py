2b176cd0d134a33ae45e5fdcc94daed82b0dafaf
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-11-21 12:23:10 -0800

    Update 1.py

--- data_Aug_txt/1.py
+++ data_Aug_txt/1.py
@@ -3,11 +3,27 @@ nltk.download('averaged_perceptron_tagger')
 
 
 
+sample_data = {
+    'A': ['The cat sleeps on the mat', 'A dog chased the ball', 'Fish swim in the ocean', 
+          'Cats like to play with yarn', 'Dogs are great companions', 'A fish tank is calming',
+          'The dog guards the house', 'Cats purr when happy', 'Fish can be colorful',
+          'A playful dog', 'A lazy cat', 'Tropical fish are beautiful'],
+    'B': ['cat', 'dog', 'fish', 'cat', 'dog', 'fish', 'dog', 'cat', 'fish', 'dog', 'cat', 'fish']
+}
+
+
+df_sample = pd.DataFrame(sample_data)
 
 
 
 
-words = sentence.split()
-    for _ in range(n):
-        random_word = random.choice(words)
-        sentence = sentence.replace(random_word, "<mask>", 1)
+
+
+
+label_target_counts = {'cat': 1000, 'dog': 1000, 'fish': 1000}
+
+
+
+
+duplication_df.head(), masking_df.head()
+
