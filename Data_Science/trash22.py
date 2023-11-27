508f20ab2dac31b5658c57982935505678f3ed4e
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-11-27 10:03:08 -0800

    Update trash22.py

--- Data_Science/trash22.py
+++ Data_Science/trash22.py
@@ -4,3 +4,14 @@ for column in df.columns:
         unique_values = df[column].unique()
         if 'YES' in unique_values or 'NO' in unique_values:
             print(f"Column '{column}' contains 'YES'/'NO' values.")
+
+
+rows_with_yes = []
+
+
+for index, row in df.iterrows():
+    if 'YES' in row.values:
+        rows_with_yes.append(index)
+
+
+print("Rows containing 'YES':", rows_with_yes)
