d4c3cceeff568a5fd4ce51070b9259e3c9f9b5ce
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-12-01 08:29:09 -0800

    Update check_ke_common_nabashe.py

--- Data_Science/check_ke_common_nabashe.py
+++ Data_Science/check_ke_common_nabashe.py
@@ -1,24 +1,30 @@
 import pandas as pd
 
-def test_and_extract_common_entries(file1, file2, columns):
-   
+def trash(file1, file2, columns):
+    
     df1 = pd.read_csv(file1)
     df2 = pd.read_csv(file2)
 
     
-    df1_selected = df1[columns]
-    df2_selected = df2[columns]
-
-   
-    common = pd.merge(df1_selected, df2_selected, on=columns, how='inner')
+    common = pd.merge(df1[columns], df2[columns], how='inner').drop_duplicates()
 
     
-    common_in_df1 = pd.merge(df1, common, on=columns, how='inner')
-    common_in_df2 = pd.merge(df2, common, on=columns, how='inner')
+    count_df1, count_df2 = 0, 0
 
-   e
-    count_df1 = common_in_df1.shape[0]
-    count_df2 = common_in_df2.shape[0]
+    
+    common_in_df1 = pd.DataFrame(columns=df1.columns)
+    common_in_df2 = pd.DataFrame(columns=df2.columns)
+
+    
+    for _, row in common.iterrows():
+        matched_df1 = df1[(df1[columns] == row.values).all(axis=1)]
+        matched_df2 = df2[(df2[columns] == row.values).all(axis=1)]
+
+        count_df1 += matched_df1.shape[0]
+        count_df2 += matched_df2.shape[0]
+
+        common_in_df1 = pd.concat([common_in_df1, matched_df1], ignore_index=True)
+        common_in_df2 = pd.concat([common_in_df2, matched_df2], ignore_index=True)
 
     print(f"Number of common rows in File 1: {count_df1}")
     print(f"Number of common rows in File 2: {count_df2}")
@@ -26,9 +32,3 @@ def test_and_extract_common_entries(file1, file2, columns):
     return common_in_df1, common_in_df2
 
 
-columns_to_check = ['A', 'B', 'C']
-common_in_file1, common_in_file2 = test_and_extract_common_entries('file1.csv', 'file2.csv', columns_to_check)
-print("Common in File 1:")
-print(common_in_file1)
-print("\nCommon in File 2:")
-print(common_in_file2)
