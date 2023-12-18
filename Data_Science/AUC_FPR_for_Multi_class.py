68a5eab311a26944f7dd1dc4b8611a4a5cfd36bc
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-12-18 14:00:14 -0800

    Update AUC_FPR_for_Multi_class.py

--- Data_Science/AUC_FPR_for_Multi_class.py
+++ Data_Science/AUC_FPR_for_Multi_class.py
@@ -1,43 +1,48 @@
 from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
-import numpy as np
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 
-true = np.array(['cat', 'fish', 'dog', 'cat', 'cat', 'fish', 'dog'])
-pred_probs = np.array([
-    [0.1, 0.6, 0.3],  # Predicted probabilities for 'fish'
-    [0.2, 0.7, 0.1],  # Predicted probabilities for 'fish'
-    [0.3, 0.4, 0.3],  # Predicted probabilities for 'fish'
-    [0.7, 0.1, 0.2],  # Predicted probabilities for 'cat'
-    [0.3, 0.3, 0.4],  # Predicted probabilities for 'dog'
-    [0.2, 0.2, 0.6],  # Predicted probabilities for 'dog'
-    [0.1, 0.3, 0.6],  # Predicted probabilities for 'dog'
-])
+true = ['cat', 'fish', 'dog', 'cat', 'cat', 'fish', 'dog']
+preds = ['fish', 'fish', 'fish', 'cat', 'dog', 'dog', 'dog']
 
 
-classes = ['cat', 'dog', 'fish']
+probabilities = [
+    [0.1, 0.6, 0.3],  # Probability for first sample
+    [0.1, 0.7, 0.2],
+    [0.2, 0.5, 0.3],
+    [0.7, 0.1, 0.2],
+    [0.2, 0.3, 0.5],
+    [0.3, 0.3, 0.4],
+    [0.2, 0.4, 0.4]
+]
 
 
-true_binarized = label_binarize(true, classes=classes)
+label_mapping = {'cat': 0, 'dog': 1, 'fish': 2}
+true_numerical = [label_mapping[label] for label in true]
+
+
+n_classes = 3
+true_binarized = label_binarize(true_numerical, classes=range(n_classes))
 
 
 fpr = dict()
 tpr = dict()
 roc_auc = dict()
-n_classes = len(classes)
 
 for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(true_binarized[:, i], pred_probs[:, i])
+    fpr[i], tpr[i], _ = roc_curve(true_binarized[:, i], [p[i] for p in probabilities])
     roc_auc[i] = auc(fpr[i], tpr[i])
 
 
-plt.figure(figsize=(8, 6))
-colors = ['blue', 'green', 'red']
+colors = cycle(['blue', 'red', 'green'])
+class_names = ['cat', 'dog', 'fish']
+plt.figure(figsize=(10, 8))
+
 for i, color in zip(range(n_classes), colors):
     plt.plot(fpr[i], tpr[i], color=color, lw=2,
-             label='ROC curve of class {0} (area = {1:0.2f})'
-             ''.format(classes[i], roc_auc[i]))
+             label=f'ROC curve of class {class_names[i]} (area = {roc_auc[i]:.2f})')
 
 plt.plot([0, 1], [0, 1], 'k--', lw=2)
 plt.xlim([0.0, 1.0])
