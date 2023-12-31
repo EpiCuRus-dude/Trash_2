64a20536df5d0aeb95c80881d504d7a84e133016
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2023-12-31 08:16:38 -0800

    Create weighted_vote.py

---
+++ similarity_search_vector/weighted_vote.py
@@ -0,0 +1,22 @@
+from scipy.special import softmax
+
+def predict_category_weighted(new_title, k=5):
+    new_vector = vectorizer.transform([new_title]).toarray().astype(np.float32)
+    distances, indices = index.search(new_vector, k=k)
+
+    
+    weights = 1 / (1 + distances[0])  # Adding 1 to avoid division by zero
+    weights = softmax(weights)  # Normalize the weights
+
+    
+    weighted_category_votes = {}
+    for idx, weight in zip(indices[0], weights):
+        category = categories[idx]
+        weighted_category_votes[category] = weighted_category_votes.get(category, 0) + weight
+
+    most_common_category = max(weighted_category_votes, key=weighted_category_votes.get)
+    return most_common_category
+
+
+predicted_category = predict_category_weighted(new_title, k=5)
+print(f"The predicted category for '{new_title}' is {predicted_category}")
