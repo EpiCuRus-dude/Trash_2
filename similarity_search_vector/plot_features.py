e5789f61da0200de7f0ecba9be662b561c9f4d19
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-01-04 09:54:18 -0800

    Create plot_features.py

---
+++ similarity_search_vector/plot_features.py
@@ -0,0 +1,23 @@
+import umap
+import numpy as np
+from tqdm import tqdm
+
+embeddings = chroma_collection.get(include=['embeddings'])['embeddings']
+umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings)
+
+
+def project_embeddings(embeddings, umap_transform):
+    umap_embeddings = np.empty((len(embeddings),2))
+    for i, embedding in enumerate(tqdm(embeddings)): 
+        umap_embeddings[i] = umap_transform.transform([embedding])
+    return umap_embeddings   
+
+projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10)
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('Projected Embeddings')
+plt.axis('off')
