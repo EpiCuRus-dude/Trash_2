70cbf8adb0374b901c2bab94e410026ac98e5978
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-02-20 15:52:58 -0800

    Update remove_trash.py

--- feature_engineering/remove_trash.py
+++ feature_engineering/remove_trash.py
@@ -18,7 +18,8 @@ def preprocess_text(text):
     tokens = word_tokenize(text)
     
     
-    extended_stopwords = set(stopwords.words('english')) | {"and", "or"}
+    extended_stopwords = set(stopwords.words('english')) | {"and", "or", "but", "for", "nor", "so", "yet", "because", "although", "though", "if", "since", "unless", "while", "whereas", "both", "either", "neither", "not only"}
+
     
     
     filtered_tokens = [word for word in tokens if word not in extended_stopwords]
