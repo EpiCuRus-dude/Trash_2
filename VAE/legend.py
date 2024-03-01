03df1ad0fb3bba9b7ba4c880211b228ddfd5b086
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-03-01 14:38:47 -0800

    Update legend.py

--- VAE/legend.py
+++ VAE/legend.py
@@ -1,4 +1,4 @@
-
+# Not related to here!
 legend_labels = [f'Label {label}: Color {color}' for label, color in label_color_map.items()]
 handles = [plt.Line2D([0], [0], marker='o', color='w', label=lbl,
                       markerfacecolor=color, markersize=10) for lbl, color in zip(legend_labels, label_color_map.values())]
