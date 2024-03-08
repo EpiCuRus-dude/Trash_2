9f262fbcb8c28c248db67cbd9284858c4073b19d
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-03-08 14:59:09 -0800

    Update 12.py

--- Algorithm/12.py
+++ Algorithm/12.py
@@ -3,3 +3,4 @@ https://leetcode.com/problems/basic-calculator-iii/submissions/1193201825/?envTy
 https://leetcode.com/problems/find-the-duplicate-number/description/
 https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/description/?envType=study-plan-v2&envId=premium-algo-100
 https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
+https://leetcode.com/problems/find-permutation/editorial/?envType=study-plan-v2&envId=premium-algo-100
