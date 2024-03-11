51d96d21791d35fa43b3d2f3546e7aede6666d3f
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-03-11 15:24:36 -0700

    Update 12.py

--- Algorithm/12.py
+++ Algorithm/12.py
@@ -5,3 +5,4 @@ https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/descri
 https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
 https://leetcode.com/problems/find-permutation/editorial/?envType=study-plan-v2&envId=premium-algo-100
 https://leetcode.com/problems/maximum-average-subarray-ii/description/?envType=study-plan-v2&envId=premium-algo-100
+https://leetcode.com/problems/factor-combinations/?envType=study-plan-v2&envId=premium-algo-100
