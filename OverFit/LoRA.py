b01fecfefb100f8987a016dbff39c1bd7e7178c6
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-03-18 08:24:48 -0700

    Update LoRA.py

--- OverFit/LoRA.py
+++ OverFit/LoRA.py
@@ -5,3 +5,6 @@ l1_penalty = sum(p.abs().sum() for p in model.parameters())
 l1_penalty = model.lora_A.abs().sum() + model.lora_B.abs().sum()
 
 l2_penalty = sum(torch.sum(param ** 2) for param in model.parameters() if param.requires_grad)
+
+
+# LLMs can easily get overfitted because of LoRA
