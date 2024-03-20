199e15b82a213cd37cb913ba7347a2c7cb64829d
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-03-20 12:43:12 -0700

    Update 12.py

--- Algorithm/12.py
+++ Algorithm/12.py
@@ -15,4 +15,5 @@ https://leetcode.com/problems/lru-cache/
 https://leetcode.com/problems/lfu-cache/description/
 https://leetcode.com/problems/design-in-memory-file-system/description/
 https://leetcode.com/problems/max-stack/editorial/
+https://leetcode.com/problems/design-hit-counter/editorial/
 
