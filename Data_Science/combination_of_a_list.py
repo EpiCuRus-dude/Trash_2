8b1de54fc04c8424cd755ebd2b7c9143fd005025
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-04-06 10:56:08 -0700

    Update combination_of_a_list.py

--- Data_Science/combination_of_a_list.py
+++ Data_Science/combination_of_a_list.py
@@ -1,3 +1,23 @@
 def all_sublists(lst):
    
     return list(chain(*[combinations(lst, i) for i in range(1, len(lst) + 1)]))
+
+
+
+
+from itertools import chain, combinations, product
+
+def all_subsets(lst):
+    
+    return list(chain(*[combinations(lst, i) for i in range(1, len(lst) + 1)]))
+
+def combine_subsets(list_1, list_2):
+    
+    subsets_list_1 = all_subsets(list_1)
+    subsets_list_2 = all_subsets(list_2)
+
+    
+    combined_subsets = list(product(subsets_list_1, subsets_list_2))
+
+    return combined_subsets
+
