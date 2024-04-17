c918a50ac7a06b6dbc0d5c008679d288c61ce6d6
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-04-17 13:22:57 -0700

    Update 1.py

--- opt/1.py
+++ opt/1.py
@@ -1,6 +1,7 @@
 from scipy.optimize import minimize
 initial_weights = np.ones(T) / T
 constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1})
+result = differential_evolution(objective, bounds, constraints=cons)
 
 result = minimize(objective, initial_weights, method='SLSQP', bounds=bounds, constraints=constraints)
 
@@ -22,3 +23,5 @@ result = minimize(objective, initial_weights, method='SLSQP', bounds=bounds, con
 
 
   regularized_variance = variance + regularization_term
+
+cons = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1})
