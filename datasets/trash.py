5a93d25e39911039353f8620982c301cf0525f95
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-04-18 08:50:02 -0700

    Update trash.py

--- datasets/trash.py
+++ datasets/trash.py
@@ -1,5 +1,5 @@
-from torchtext.datasets import YahooAnswers
-train_iter, test_iter = YahooAnswers(split=('train', 'test'))
+from torchtext.datasets import ....
+train_iter, test_iter = ...(split=('train', 'test'))
 
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
@@ -14,4 +14,34 @@ vocab.set_default_index(vocab["<unk>"])
 
 text_pipeline = lambda x: [vocab[token] for token in tokenizer(x)]
 label_pipeline = lambda x: int(x)
+
+import csv
+
+
+
+def save_to_csv(data_iter, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['label', 'text'])  # writing the headers
+        for label, text in data_iter:
+            writer.writerow([label, text])
+
+train_iter, test_iter = ...(split=('train', 'test'))
+save_to_csv(train_iter, '..._train.csv')
+save_to_csv(test_iter, '..._test.csv')
+
+import pandas as pd
+
+def data_to_dataframe(data_iter):
+    # Create a DataFrame directly from the iterator
+    df = pd.DataFrame([(label, text) for label, text in data_iter], columns=['label', 'text'])
+    return df
+
+# Use the previously saved CSV or create a DataFrame directly
+df_train = data_to_dataframe(train_iter)
+df_train.to_csv('..._train_dataframe.csv', index=False)
+df_test = data_to_dataframe(test_iter)
+df_test.to_csv('..._test_dataframe.csv', index=False)
+
+
 
