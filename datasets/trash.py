907d2b0929cef4c4ae4049fd7770ea70e7667734
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-04-18 08:45:16 -0700

    Update trash.py

--- datasets/trash.py
+++ datasets/trash.py
@@ -1,2 +1,14 @@
 from torchtext.datasets import YahooAnswers
 train_iter, test_iter = YahooAnswers(split=('train', 'test'))
+
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+tokenizer = get_tokenizer('basic_english')
+def yield_tokens(data_iter):
+    for label, text in data_iter:
+        yield tokenizer(text)
+
+vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
+vocab.set_default_index(vocab["<unk>"])
+
