4da05e79305c933dc8024e9cb1b402c6564d74b7
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-04-22 08:50:29 -0700

    Create trash.py

---
+++ similarity_search_vector/gpu/trash.py
@@ -0,0 +1,5 @@
+
+    vector_dim = 128
+    gpu_config = faiss.GpuIndexFlatConfig()
+    gpu_config.device = 0  # Use the first GPU
+    gpu_index = faiss.GpuIndexFlatL2(gpu_resources, vector_dim, gpu_config)
