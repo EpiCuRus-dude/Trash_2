1274e63a54b0446319920ca1458047d635ed34ca
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-05-16 12:42:02 -0700

    Update trasshhh.py

--- similarity_search_vector/trasshhh.py
+++ similarity_search_vector/trasshhh.py
@@ -1,26 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def find_elbow_point(scores):
+def kneedle(scores):
     sorted_indices = np.argsort(scores)[::-1]
     sorted_scores = scores[sorted_indices]
     cumulative_scores = np.cumsum(sorted_scores)
     normalized_cumulative_scores = cumulative_scores / cumulative_scores[-1]
-    n = len(scores)
-    all_coord = np.vstack((range(n), normalized_cumulative_scores)).T
-    first_point = all_coord[0]
-    line_vec = all_coord[-1] - all_coord[0]
-    line_vec = line_vec / np.sqrt(np.sum(line_vec**2))
-    vec_from_first = all_coord - first_point
-    scalar_prod = np.sum(vec_from_first * np.tile(line_vec, (n, 1)), axis=1)
-    vec_from_first_parallel = np.outer(scalar_prod, line_vec)
-    vec_to_line = vec_from_first - vec_from_first_parallel
-    dist_to_line = np.sqrt(np.sum(vec_to_line ** 2, axis=1))
-    elbow_point = np.argmax(dist_to_line)
-    return elbow_point + 1, sorted_indices[:elbow_point + 1]
+    
+    differences = np.diff(normalized_cumulative_scores)
+    diff_signs = np.sign(differences)
+    knee_point = np.argmax(diff_signs < 0) + 1
+    
+    return knee_point, sorted_indices[:knee_point]
 
 scores = np.array([85, 92, 88, 96, 80, 78, 84, 93, 91, 87])
-optimal_candidates, top_indices = find_elbow_point(scores)
+optimal_candidates, top_indices = kneedle(scores)
 
 print(f"Optimal number of top candidates to choose: {optimal_candidates}")
 print(f"Indices of top candidates: {top_indices}")
@@ -33,5 +27,5 @@ plt.plot(range(1, len(scores) + 1), normalized_cumulative_scores, marker='o')
 plt.axvline(x=optimal_candidates, color='r', linestyle='--')
 plt.xlabel('Number of Candidates')
 plt.ylabel('Normalized Cumulative Score')
-plt.title('Elbow Method for Optimal Number of Candidates')
+plt.title('Kneedle Method for Optimal Number of Candidates')
 plt.show()
