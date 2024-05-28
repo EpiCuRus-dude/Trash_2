83ab27b7e24b3d4d21e0bcfee9a103205f304ad0
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-05-27 17:07:48 -0700

    Update keyyys.py

--- dicom/keyyys.py
+++ dicom/keyyys.py
@@ -1,19 +1,19 @@
 import itertools
 
 def generate_combinations(keys, numbers):
-
     all_combinations = []
 
-
+    # Generate combinations with different lengths
     for r_keys in range(1, len(keys) + 1):
         for r_numbers in range(1, len(numbers) + 1):
             key_combos = list(itertools.combinations(keys, r_keys))
             number_combos = list(itertools.combinations(numbers, r_numbers))
             for key_combo in key_combos:
                 for number_combo in number_combos:
-                    all_combinations.append(key_combo + number_combo)
+                    all_combinations.append([key_combo, number_combo])
     
     return all_combinations
+
 
 
 keys = ['key1', 'key2', 'key3']
