d7e7e33ef57751679fee1792be434d8226927ce6
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-06-04 14:41:22 -0700

    Update Stupid.py

--- Data_Science/Stupid.py
+++ Data_Science/Stupid.py
@@ -1,21 +1,32 @@
 #
 import os
 
-def collect_nn_files(root_dir):
+import os
+
+def collect_nn_files(root_dir, expected_postfix):
     nn_files_dict = {}
 
-    for entry in os.scandir(root_dir):
-        if entry.is_dir() and '_' in entry.name:
-            prefix = entry.name.split('_')[0]
-            sub_dir_path = entry.path
-            
-            nn_files = []
-            for sub_entry in os.scandir(sub_dir_path):
-                if sub_entry.is_file() and sub_entry.name.endswith('.nn'):
-                    nn_files.append(sub_entry.path)
+    def scan_directory(directory):
+        for entry in os.scandir(directory):
+            if entry.is_dir():
+                if '_' in entry.name:
+                    prefix, postfix = entry.name.split('_', 1)
+                    if postfix == expected_postfix:
+                        sub_dir_path = entry.path
 
-            if prefix not in nn_files_dict:
-                nn_files_dict[prefix] = []
-            nn_files_dict[prefix].extend(nn_files)
+                        nn_files = []
+                        for sub_entry in os.scandir(sub_dir_path):
+                            if sub_entry.is_file() and sub_entry.name.endswith('.nn'):
+                                nn_files.append(sub_entry.path)
+                            elif sub_entry.is_dir():
+                                scan_directory(sub_entry.path)  # Recursively scan subdirectories
 
+                        if prefix not in nn_files_dict:
+                            nn_files_dict[prefix] = []
+                        nn_files_dict[prefix].extend(nn_files)
+                else:
+                    scan_directory(entry.path)  # Recursively scan subdirectories without the correct postfix
+
+    scan_directory(root_dir)
     return nn_files_dict
+
