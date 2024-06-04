7822bd9217a2ec9663b612abc29bb35ceca83662
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-06-04 14:58:13 -0700

    Update Stupid.py

--- Data_Science/Stupid.py
+++ Data_Science/Stupid.py
@@ -1,32 +1,20 @@
-#
-import os
 
-import os
+def compare_dicom_files(file1_path, file2_path):
 
-def collect_nn_files(root_dir, expected_postfix):
-    nn_files_dict = {}
+    dicom1 = pydicom.dcmread(file1_path)
+    dicom2 = pydicom.dcmread(file2_path)
 
-    def scan_directory(directory):
-        for entry in os.scandir(directory):
-            if entry.is_dir():
-                if '_' in entry.name:
-                    prefix, postfix = entry.name.split('_', 1)
-                    if postfix == expected_postfix:
-                        sub_dir_path = entry.path
 
-                        nn_files = []
-                        for sub_entry in os.scandir(sub_dir_path):
-                            if sub_entry.is_file() and sub_entry.name.endswith('.nn'):
-                                nn_files.append(sub_entry.path)
-                            elif sub_entry.is_dir():
-                                scan_directory(sub_entry.path)  # Recursively scan subdirectories
+    dicom1_dict = {tag: dicom1[tag].value for tag in dicom1.dir()}
+    dicom2_dict = {tag: dicom2[tag].value for tag in dicom2.dir()}
 
-                        if prefix not in nn_files_dict:
-                            nn_files_dict[prefix] = []
-                        nn_files_dict[prefix].extend(nn_files)
-                else:
-                    scan_directory(entry.path)  # Recursively scan subdirectories without the correct postfix
 
-    scan_directory(root_dir)
-    return nn_files_dict
+    differences = []
 
+    all_keys = set(dicom1_dict.keys()).union(dicom2_dict.keys())
+
+    for key in all_keys:
+        value1 = dicom1_dict.get(key, None)
+        value2 = dicom2_dict.get(key, None)
+        if value1 != value2:
+            differences.append((key, value1, value2))
