6274496aa4c345a4d9379a2b641c89a8369114f1
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-06-27 14:46:41 -0700

    Update Stupid.py

--- Data_Science/Stupid.py
+++ Data_Science/Stupid.py
@@ -41,3 +41,35 @@ def extract_and_order_segments(text, keywords):
                 used_segments.add(potential_segment)
     return segment_dict
 
+
+
+
+def extract_and_order_segments(text, keywords):
+    keywords_regex = r'(' + '|'.join(re.escape(keyword) for keyword in keywords) + r')(?!\w)'
+    matches = list(re.finditer(keywords_regex, text))
+    segment_dict = {}
+    used_segments = set()
+    for i in range(len(matches) - 1):
+        start = matches[i]
+        end = matches[i + 1]
+        potential_segment = text[start.end():end.start()].strip()
+        if not re.search(keywords_regex, potential_segment):
+            segment_key = f"{start.group(0)}_{end.group(0)}"
+            if potential_segment not in used_segments:
+                segment_dict[segment_key] = potential_segment
+                used_segments.add(potential_segment)
+    
+    
+    if matches:
+        last_match = matches[-1]
+        last_segment = text[last_match.end():].strip()
+        if not re.search(keywords_regex, last_segment):
+            last_key = f"{last_match.group(0)}_end"
+            if last_segment not in used_segments:
+                segment_dict[last_key] = last_segment
+                used_segments.add(last_segment)
+                
+    return segment_dict
+
+
+
