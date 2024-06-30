831ba36054c3225404e1663817a772331ad352a0
Imicurus <91218774+EpiCuRus-dude@users.noreply.github.com>
2024-06-30 12:06:29 -0700

    Update Stupid.py

--- Data_Science/Stupid.py
+++ Data_Science/Stupid.py
@@ -1,37 +1,19 @@
 import re
+import nltk
+from nltk.tokenize import sent_tokenize
 
-def extract_pirads(text):
-    
-    match = re.search(r"PI[-\s]?RADS\D{0,10}(\d)", text)
-    if match:
-        return int(match.group(1))
-    else:
-        return None  
 
-import spacy
-from spacy.training import Example
 
-nlp = spacy.blank("en")
 
-if "ner" not in nlp.pipe_names:
-    ner = nlp.create_pipe("ner")
-    nlp.add_pipe("ner", last=True)
-else:
-    ner = nlp.get_pipe("ner")
+def find_pirads_sentences(text):
+    # Tokenize the text into sentences
+    sentences = sent_tokenize(text)
 
-ner.add_label("PI-RADS")
+    pirads_sentences = [sentence for sentence in sentences if re.search(r"\bPI[-\s]?RADS\b", sentence, re.IGNORECASE)]
+    return pirads_sentences
 
-train_data = [
-    ("The patient has a PI-RADS 4 lesion.", {"entities": [(21, 29, "PI-RADS")]}),
-    ("PI-RADS: 5 indicates high likelihood of malignancy.", {"entities": [(0, 8, "PI-RADS")]}),
-]
 
-optimizer = nlp.resume_training()
-for itn in range(10):
-    for text, annotations in train_data:
-        doc = nlp.make_doc(text)
-        example = Example.from_dict(doc, annotations)
-        nlp.update([example], drop=0.5, sgd=optimizer)
-
-nlp.to_disk("/path/to/model")
-
+pirads_sentences = find_pirads_sentences(text)
+print("Sentences containing 'dddd':")
+for sentence in pirads_sentences:
+    print(sentence)
